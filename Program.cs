d9eb3ba6f88580ffff15ff21b723b0fd073ef739
riabkovalentyn <136009615+riabkovalentyn@users.noreply.github.com>
2023-11-26 15:42:44 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,17 +5,27 @@ using Newtonsoft.Json;
 
 public class Program{
     public static void Main(){
-        ITEmployee linuxGuy = new ITEmployee("Rico", "Marko", "LinuxEmployee");
-        linuxGuy.AddTool(new Notebook());
-        linuxGuy.AddTool(new Phone());
-        linuxGuy.Work();
-        try{
-        string jsonFilePath = "#/data.json";
-        string jsonData = File.ReadAllText(jsonFilePath);
-        dynamic data = JsonConvert.DeserializeObject(jsonData);
-        Console.WriteLine(data);
-        } catch (Exception ex} {
-        Console.WriteLine("Error" + ex.Message) ;
+        Equipment office = new Equipment("001", "Office");
+        Equipment Notebook = new Equipment("002","NoteBook");
+        Equipment Phone = new Equipment("003","Phone");   
+
+        LinuxExpert john = new LinuxExpert("John");
+        Developer kristian = new Developer("Kristian");
+        Logistics mike = new Logistics("Mike");
+        Accountant emma = new Accountant("Emma");
+        Manager tom = new Manager("Tom");
+        Company company = new Company("XYZ Company");
+        company.AddEmployee(john);
+        company.AddEmployee(sarah);
+        company.AddEmployee(mike);
+        company.AddEmployee(emma);
+        company.AddEmployee(tom);
+
+        company.AssignEquipment(john, office);
+        company.AssignEquipment(sarah, notebook);
+        company.AssignEquipment(mike, phone);
+
+        company.ListEmployeesWithEquipment();
      }
   }
 }
